065e9258ac77e6e873e38a97278d6f6cec0e2c05
schubert <schubertyi@gmail.com>
2019-09-12 23:28:09 -0300

    testing

--- dijkstra_algorithm/main.py
+++ dijkstra_algorithm/main.py
@@ -1,15 +1,16 @@
-from graph import Graph
 from pathlib import Path
-
-
-
-
-
+from algorithm import dijkstra
+from graph import Graph
 
 def Main():
   graph = Graph(graphFile)
   graph.getNodes()
-  graph.showGraph()
+  graph.setObjective(1, 4)
+  graph.printGraph()
+  dijkstra(graph)
+  graph.printGraph()
+  path = graph.getPath()
+  print(path)
 
 if __name__ == "__main__":
   graphFile = Path('graph')
