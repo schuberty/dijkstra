ea18803ae3a574f55db71152ff017f1d6d48ae41
schubert <schubertyi@gmail.com>
2019-09-12 23:25:33 -0300

    only ignoring path value if not 0

--- dijkstra_algorithm/algorithm.py
+++ dijkstra_algorithm/algorithm.py
@@ -7,5 +7,15 @@ def allClosed(graph):
   return True
 
 def dijkstra(graph):
-  graph.printGraph()
-  # while(not(allClosed(graph))):
+  while(not(allClosed(graph))):
+    selected = graph.getLessEstimate()
+    graph.nodes[selected].close()
+    for i in range(len(graph.nodes[selected].point)):
+      if(graph.nodes[selected] is graph.nodes[i]):
+        continue
+      else:
+        if(not(graph.nodes[i].closed) and graph.nodes[selected].point[i] != 0):
+          sumEstimate = graph.nodes[selected].estimate + graph.nodes[selected].point[i]
+          if(sumEstimate < graph.nodes[i].estimate):
+            graph.nodes[i].estimate = sumEstimate
+            graph.nodes[i].ancestor = selected
