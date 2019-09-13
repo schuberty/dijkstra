523e8fa472e2ddbbc4a7c2dad533b56658219904
schubert <schubertyi@gmail.com>
2019-09-13 16:43:00 -0300

    node print mode converted from number to letters

--- dijkstra_algorithm/node.py
+++ dijkstra_algorithm/node.py
@@ -9,7 +9,7 @@ class Node:
   
   def printNode(self):
     # print(" NODE - TO - CLOSED - ANCESTOR - ESTIMATE")
-    print(str(self.point) + " " + str(self.closed) + " " + str(self.ancestor) + " " + str(self.estimate))
+    print(str(self.point) + " " + str(self.closed) + " " + str(chr(self.ancestor+65)) + " " + str(self.estimate))
 
   def close(self):
     self.closed = True
