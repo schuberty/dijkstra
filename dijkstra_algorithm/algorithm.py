70dedc43a59c8fed2fe7787dfbb63bdcf469e9c8
schubert <schubertyi@gmail.com>
2019-09-13 22:09:48 -0300

    getLessEstimate moved to here

--- dijkstra_algorithm/algorithm.py
+++ dijkstra_algorithm/algorithm.py
@@ -1,21 +1,28 @@
 from graph import Graph
+inf = float('inf')
 
-def allClosed(graph):
+def checkAllClosed(graph):
   for i in range(len(graph.nodes)):
     if(graph.nodes[i].closed == False):
       return False
   return True
 
+def getLessEstimate(graph):
+  min = inf
+  index = 0
+  for i in range(len(graph.nodes)):
+    if(graph.nodes[i].estimate < min and not(graph.nodes[i].closed)):
+      min = graph.nodes[i].estimate
+      index = i
+  return index
+
 def dijkstra(graph):
-  while(not(allClosed(graph))):
-    selected = graph.getLessEstimate()
+  while(not(checkAllClosed(graph))):
+    selected = getLessEstimate(graph)
     graph.nodes[selected].close()
     for i in range(len(graph.nodes[selected].point)):
-      if(graph.nodes[selected] is graph.nodes[i]):
-        continue
-      else:
-        if(not(graph.nodes[i].closed) and graph.nodes[selected].point[i] != 0):
-          sumEstimate = graph.nodes[selected].estimate + graph.nodes[selected].point[i]
-          if(sumEstimate < graph.nodes[i].estimate):
-            graph.nodes[i].estimate = sumEstimate
-            graph.nodes[i].ancestor = selected
+      if(not(graph.nodes[selected] is graph.nodes[i]) and not(graph.nodes[i].closed) and graph.nodes[selected].point[i] != 0):
+        sumEstimate = graph.nodes[selected].estimate + graph.nodes[selected].point[i]
+        if(sumEstimate < graph.nodes[i].estimate):
+          graph.nodes[i].estimate = sumEstimate
+          graph.nodes[i].ancestor = selected
